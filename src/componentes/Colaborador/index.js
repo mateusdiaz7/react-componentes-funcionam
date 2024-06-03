f3f77df441d9668db0b46c6b0efe23345bbae74d
Mateus Dias <124614766+mateusdiaz7@users.noreply.github.com>
2024-06-03 17:29:04 -0300

    finalizando o projeto organo

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,37 @@
+import { IoMdCloseCircle } from "react-icons/io";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar}) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsfavorito = {
+        size: 25,
+        onClick: favoritar,
+        color: 'red'
+    }
     return (<div className="colaborador">
+            <IoMdCloseCircle
+                onClick={() => aoDeletar(colaborador.id)}
+                size={25}
+                className="icone-deletar"
+            />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className="favoritar">
+                {colaborador.favorito
+                    ? <FaHeart {...propsfavorito}/> 
+                    : <FaRegHeart {...propsfavorito} />
+                }
+            </div>
         </div>
     </div>)
 }
 
-export default Colaborador
+export default Colaborador;
